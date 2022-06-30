d147abbdbd5b00bdce7046bbfaa7be4149833d3a
Sergiy Mashtaler <svmashtaler@gmail.com>
2022-06-30 13:43:03 +0200

    init

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from typing import Union
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get('/')
+def read_root():
+    """Read root"""
+    return {'Hello': 'World'}
+
+
+@app.get('/items/{item_id}')
+def read_item(item_id: int, query: Union[str, None] = None):
+    """Get item by ID"""
+    return {'item_id': item_id, 'query': query}
